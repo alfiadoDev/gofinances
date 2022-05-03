fe03901220fb7a56987dc30d72179ae08578bf9d
Alfiado Constantino <alfiado.ngana@xit2s.com>
2022-05-03 20:57:57 +0200

    authentication interface

--- App.tsx
+++ App.tsx
@@ -16,6 +16,7 @@ import {
 import { AppRoutes } from './src/routes/app.routes';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { StatusBar } from 'react-native';
+import { SignIn } from './src/screens/SignIn';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -33,7 +34,7 @@ export default function App() {
       <ThemeProvider theme={theme}>
         <NavigationContainer>
           <StatusBar barStyle='light-content' />
-          <AppRoutes />
+          <SignIn />
         </NavigationContainer>
       </ThemeProvider>
     </GestureHandlerRootView>
