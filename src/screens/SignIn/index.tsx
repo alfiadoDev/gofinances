a1403732651b34321683e22a3984a115d0db1a64
Alfiado Constantino <alfiado.ngana@xit2s.com>
2022-05-06 15:48:30 +0200

    authentication options in platform

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { ActivityIndicator, Alert } from "react-native";
+import { ActivityIndicator, Alert, Platform } from "react-native";
 import { RFValue } from "react-native-responsive-fontsize";
 import { useTheme } from 'styled-components';
 
@@ -78,11 +78,11 @@ export function SignIn () {
             svg={GoogleSvg}
             onPress={handleSignInWithGoogle}
           />
-          <SignInSocialButton 
+          {Platform.OS === 'ios' && <SignInSocialButton 
             title="Entrar com Apple"
             svg={AppleSvg}
             onPress={handleSignInWithApple}
-          />
+          />}
         </FooterWrapper>
 
         { isLoading && <ActivityIndicator 
